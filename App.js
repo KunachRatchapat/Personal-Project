bc379498cd5dc615b517cac0940ee78a3cddb3a2
Kunach R. <146532637+lilTeh@users.noreply.github.com>
2025-02-05 18:57:53 +0700

    Finish Input

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./src/Home";
+import AddItem from "./src/AddItem";
 
 
 const Stack = createStackNavigator();
@@ -9,9 +9,8 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ title: "หน้าหลัก" }} />
-    
+      <Stack.Navigator initialRouteName="AddItem">
+        <Stack.Screen name="AddItem" component={AddItem}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/AddItem.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
+
+const AddItem = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleSearch = () => {
+    if (!name || !price || image) {
+      Alert.alert("กรุณากรอกข้อมูลให้ครบ");
+    } else {
+      const newItem = {
+        id: Date.now().toString(),
+        name,
+        price,
+        image,
+      };
+
+      console.log(newItem); // เก็บค่าไอเทม
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Add New Product</Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Product Name"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Product Price"
+        value={price}
+        onChangeText={setPrice}
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Product Image URL"
+        value={image}
+        onChangeText={setImage}
+      />
+
+      <Button title="Add Product" color="green" onPress={handleSearch} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({  
+  container: {  
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#D3D3D3",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    marginBottom: 10,
+  },
+});
+
+export default AddItem;
